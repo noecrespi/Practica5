8a899be61ef411f715403e413e2e9c327af9ba66
Joan Nicolau <joannicolau23@gmail.com>
2022-05-09 18:42:19 +0200

    finish tests

--- src/test/java/edu/poniperro/CalculadoraTest.java
+++ src/test/java/edu/poniperro/CalculadoraTest.java
@@ -18,6 +18,23 @@ public class CalculadoraTest {
     @Test
     public void testSuma() {
         assertEquals(4, calculadora.sumar(2, 2), DELTA);
+        assertEquals(-4, calculadora.sumar(-2, -2), DELTA);
+    }
+
+    @Test
+    public void testResta() {
+        assertEquals(4, calculadora.restar(6, 2), DELTA);
+        assertEquals(-8, calculadora.restar(-10, -2), DELTA);
+    }
+
+    @Test
+    public void testMultiplicacion() {
+        assertEquals((long)1000, (long)calculadora.multiplicar(100, 10));
+    }
+
+    @Test
+    public void testDivisio() {
+        assertEquals((long)100, (long)calculadora.dividir(1000, 10));
     }
     
 }
