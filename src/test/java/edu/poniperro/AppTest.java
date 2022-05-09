b4988cbf8817752de455477fd19fd0b436a82d6e
Joan Nicolau <joannicolau23@gmail.com>
2022-05-09 17:17:48 +0200

    initial build

---
+++ src/test/java/edu/poniperro/AppTest.java
@@ -0,0 +1,12 @@
+package edu.poniperro;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
