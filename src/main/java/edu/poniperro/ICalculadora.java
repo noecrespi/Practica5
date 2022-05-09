7fc3915a9ec1f1746acddfb740308d5013e4e3d1
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-05-09 18:00:34 +0200

    add calculator interface

---
+++ src/main/java/edu/poniperro/ICalculadora.java
@@ -0,0 +1,12 @@
+package edu.poniperro;
+
+public interface ICalculadora {
+
+    //Métodos
+    double sumar(double num1, double num2);
+    double restar(double num1, double num2);
+    double multiplicar(double num1, double num2);
+    double dividir(double num1, double num2);
+
+    
+}
