70637cd81b4cb5da297e0c4a60318c2e9332a7ee
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-05-09 19:45:37 +0200

    add javadoc to calculadora methods

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Practica5 - JUnit
 
-1. Crea un nuevo proyecto en github, por nombre Practica5.
-2. Crea la interfaz ICalculadora con las operaciones sumar, restar,
+1. Crear proyecto en github (Practica5).
+2. Crear la interfaz ICalculadora con las operaciones sumar, restar,
 multiplicar y dividir.
 3. Crea la clase calculadora que utilice la interfaz anterior e implementa los
 métodos.
@@ -11,3 +11,13 @@ porque puede venir incluido en vuestro IDE) de estos cuatro métodos.
 6. Realiza el segundo commit.
 7. Realiza diferentes pruebas para validar que los test funcionan y expón
 los resultados obtenidos, % de tests pasados etc.
+
+
+IMAGENES DE TESTING 
+IMAGENES DEL VERSIONADO SEMANTICO 
+JAVA DOC
+
+CREAR UN ANUEVA RAMA DEVELOP DONDE CREAMOS ALLI LA CLASE Y LA INTERFAZ CALCULADORA 
+CREAR  UNA RAMA TESTING PARA HACER TODO LOS TEST 
+
+VERSIONADO SEMANTICO IMG
--- src/main/java/edu/poniperro/Calculadora.java
+++ src/main/java/edu/poniperro/Calculadora.java
@@ -2,21 +2,45 @@ package edu.poniperro;
 
 public class Calculadora implements ICalculadora{
 
+    
+    /** coge dos numeros y devuelve su suma
+     * @param num1
+     * @param num2
+     * @return double
+     */
     @Override
     public double sumar(double num1, double num2) {
         return num1 + num2;
     }
 
+    
+    /** coge dos numeros y devuelve su resta
+     * @param num1
+     * @param num2
+     * @return double
+     */
     @Override
     public double restar(double num1, double num2) {
         return num1 - num2;
     }
 
+    
+    /** coge dos numeros y devuelve su multiplicación
+     * @param num1
+     * @param num2
+     * @return double
+     */
     @Override
     public double multiplicar(double num1, double num2) {
         return num1 * num2;
     }
 
+    
+    /** coge dos numeros y devuelve s
+     * @param num1
+     * @param num2
+     * @return double
+     */
     @Override
     public double dividir(double num1, double num2) {
         return num1 / num2;
